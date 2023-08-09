8b0290b328db38886ae4bd07fa8e68013ae8e672
Chandelier101 <79680180+Chandelier101@users.noreply.github.com>
2023-08-09 14:05:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -62,10 +62,10 @@ def handler(context: dict, request: Request) -> Response:
     # print(context)
     
     inputs = tokenizer.encode(prompt, return_tensors="pt")
-    # print(inputs.shape)
+    print(inputs.shape)
     print(inputs)
-    print("Keys:",inputs['input_ids'])
-    outputs = model.generate(inputs['input_ids'].to(device), max_new_tokens=int(max_new_tokens))
+    print("Keys:",inputs[0])
+    outputs = model.generate(inputs[0].to(device), max_new_tokens=int(max_new_tokens))
     output = tokenizer.decode(outputs[0])
 
     return Response(
